cc055b4ffc9397e1ab8d7fb46c06ec9d93db2c92
dblant <dustinblntn@gmail.com>
2020-06-17 19:09:18 -0500

    first commit

---
+++ lib/environment.rb
@@ -0,0 +1,6 @@
+require "my_test_project/version"
+
+module MyTestProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/my_test_project/version.rb
@@ -0,0 +1,3 @@
+module MyTestProject
+  VERSION = "0.1.0"
+end
