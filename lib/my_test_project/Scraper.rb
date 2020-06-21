1ca6570a84bd38a7cd8ed8543f8ff0e1ffcb2827
dblant <dustinblntn@gmail.com>
2020-06-20 22:02:53 -0500

    created path to story page while inside the original parsed enumeration

--- lib/my_test_project/Scraper.rb
+++ lib/my_test_project/Scraper.rb
@@ -3,6 +3,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'httparty'
+require 'json'
 require 'pry'
 
 
@@ -18,8 +19,11 @@ def get_page_headlines(sport)
     parsed_page.css('div.headlineStack').each do |headlines|
        page_id = headlines.css('section ul li a')[0].attributes["href"].text.split("/")[5] 
         # ^^ This gets the id for the page for the article.  We can use this to get the text from the h1 so that we can use the text for our title.
-        
-        binding.pry
+                story_url = url + "/story/_/id/" + page_id
+                unparsed_story_url = HTTParty.get(story_url)
+                parsed_story_url = Nokogiri::HTML(unparsed_story_url)
+                # ^^ This gets us to the story page where we can take the title text and assign it to the link we will create
+            binding.pry
     end
 
 
