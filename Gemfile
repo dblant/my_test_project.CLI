f672c601f4343140554242d345e8fc7eb689bd1f
dblant <dustinblntn@gmail.com>
2020-06-20 21:43:29 -0500

    added access to headline id in scraper class. added json to gems

--- Gemfile
+++ Gemfile
@@ -10,3 +10,5 @@ gem "httparty", "~> 0.18.1"
 gem 'pry'
 
 gem 'nokogiri', '~> 1.10', '>= 1.10.9'
+
+gem 'json', '~> 2.3'
--- lib/my_test_project/Scraper.rb
+++ lib/my_test_project/Scraper.rb
@@ -15,9 +15,10 @@ def get_page_headlines(sport)
     unparsed_url = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_url)
 
-    parsed_page.css('div.headlineStack').each do |headline|
+    parsed_page.css('div.headlineStack').each do |headlines|
+       page_id = headlines.css('section ul li a')[0].attributes["href"].text.split("/")[5] 
+        # ^^ This gets the id for the page for the article.  We can use this to get the text from the h1 so that we can use the text for our title.
         
-
         binding.pry
     end
 
