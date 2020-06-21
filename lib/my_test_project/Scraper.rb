22c000356982bef210a6a660ac95180f0ad88e09
dblant <dustinblntn@gmail.com>
2020-06-20 20:46:00 -0500

    parsed the espn url and created my each iteration

--- lib/my_test_project/Scraper.rb
+++ lib/my_test_project/Scraper.rb
@@ -13,8 +13,15 @@ BASE_URL = 'https://www.espn.com/'
 def get_page_headlines(sport)
     url = BASE_URL + "#{sport}"
     unparsed_url = HTTParty.get(url)
-    parse_page = Nokogiri::HTML(unparsed_url)
-    binding.pry
+    parsed_page = Nokogiri::HTML(unparsed_url)
+
+    parsed_page.css('div.headlineStack').each do |headline|
+        
+
+        binding.pry
+    end
+
+
     # This method should return the list of headlines from each sport page.
 end
 
